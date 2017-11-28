cb1f20ded90b90c55ae46f2626d052916d720785
Victor Caballero <vcaballero@salleurl.edu>
2017-11-28 18:20:38 +0100

    SiotApplicationMobility: first approach created. Needs testing. Build -> Compiling.

--- src/siot/model/siot-application-mobility.cc
+++ src/siot/model/siot-application-mobility.cc
@@ -11,34 +11,73 @@
 
 namespace ns3 {
 
-SiotApplicationMobility::SiotApplicationMobility(uint32_t range, Ptr<SiotApplication> app) {
+SiotApplicationMobility::SiotApplicationMobility(double range,
+		Ptr<SiotApplication> app) {
 	this->m_range = range;
 	this->m_app = app;
 }
 
-void SiotApplicationMobility::DoDispose (void)
-{
+void SiotApplicationMobility::Watch(
+		std::vector<Ptr<const MobilityModel>> nodes) {
+	for (auto n : nodes) {
+		m_watching.insert( { n->GetObject<Node>()->GetId(), n });
+	}
+}
+
+void SiotApplicationMobility::DoDispose(void) {
 	this->m_app = 0;
 	Application::DoDispose();
 }
-
 
 SiotApplicationMobility::~SiotApplicationMobility() {
 	// TODO Auto-generated destructor stub
 }
 
-std::vector<Ptr<Node>> SiotApplicationMobility::GetInRange()
-{
-	std::vector<Ptr<Node>> vec = {};
+std::vector<Ptr<Node>> SiotApplicationMobility::GetInRange() {
+
+	std::vector<Ptr<const MobilityModel>> inRangeVector;
+
+	for (auto n : m_watching) {
+		bool inRange;
+		double distance;
+
+		std::tie(inRange, distance) = this->IsNodeInRange(n.second);
+
+		if (inRange == true) {
+			inRangeVector.push_back(n.second);
+			if (!contains(m_inRange, n.first)) {
+				m_inRange.insert(n.first);
+				this->NotifyEntersRange(n.second);
+			}
+		} else { // inRange == false
+			remove(m_inRange, n.first);
+			this->NotifyLeavesRange(n.second);
+		}
+
+	}
+
+	std::vector<Ptr<Node>> vec = { };
 	return vec;
 }
 
-bool SiotApplicationMobility::IsNodeInRange(Ptr<Node>)
-{
+std::tuple<bool, double> SiotApplicationMobility::IsNodeInRange(
+		Ptr<const MobilityModel> other) {
+	double distance = this->m_mobilityModel->GetDistanceFrom(other);
 
+	if (distance <= this->m_range) {
+		return std::make_tuple(true, distance);
+	} else {
+		return std::make_tuple(false, distance);;
+	}
 
+}
 
-	return true;
+void SiotApplicationMobility::NotifyEntersRange(Ptr<const MobilityModel> node) {
+	this->m_nodeEntersRangeTrace(m_app, node);
+}
+
+void SiotApplicationMobility::NotifyLeavesRange(Ptr<const MobilityModel> node) {
+	this->m_nodeLeavesRangeTrace(m_app, node);
 }
 
 } /* namespace ns3 */
--- src/siot/model/siot-application-mobility.h
+++ src/siot/model/siot-application-mobility.h
@@ -10,9 +10,12 @@
 
 #include <unordered_map>
 #include <vector>
+#include <set>
+#include <tuple>
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
 #include "ns3/application.h"
 
 namespace ns3 {
@@ -21,12 +24,13 @@ class SiotApplication;
 
 class SiotApplicationMobility: public Application {
 public:
-	SiotApplicationMobility(uint32_t, Ptr<SiotApplication>);
+	SiotApplicationMobility(double, Ptr<SiotApplication>);
+	void Watch(std::vector<Ptr<const MobilityModel>>);
 	virtual ~SiotApplicationMobility();
 	std::vector<Ptr<Node>> GetInRange();
 
-	typedef void (* NodeEntersRange)(Ptr<const SiotApplication>, Ptr<const Node>);
-	typedef void (* NodeLeavesRange)(Ptr<const SiotApplication>, Ptr<const Node>);
+	typedef void (* NodeEntersRange)(Ptr<const SiotApplication>, Ptr<const MobilityModel>);
+	typedef void (* NodeLeavesRange)(Ptr<const SiotApplication>, Ptr<const MobilityModel>);
 
 protected:
 
@@ -34,16 +38,54 @@ protected:
 
 private:
 
-	bool IsNodeInRange(Ptr<Node>);
+	void NotifyEntersRange(Ptr<const MobilityModel>);
+	void NotifyLeavesRange(Ptr<const MobilityModel>);
 
-	std::uint32_t m_range;
-	std::unordered_map<std::uint32_t, Ptr<Node>> m_adjacencies;
+	std::tuple<bool, double> IsNodeInRange(Ptr<const MobilityModel>);
+	bool hasNodeEnteredRange(Ptr<const MobilityModel>);
+	bool hasNodeLeavedRange(Ptr<const MobilityModel>);
+
+	double m_range;
+	std::unordered_map<std::uint32_t, Ptr<const MobilityModel>> m_watching;
+	std::set<uint32_t> m_inRange;
+
 	Ptr<SiotApplication> m_app;
+	Ptr<MobilityModel> m_mobilityModel;
 
-	ns3::TracedCallback<Ptr<const SiotApplication>, Ptr<const Node>> m_nodeEntersRangeTrace;
-	ns3::TracedCallback<Ptr<const SiotApplication>, Ptr<const Node>> m_nodeLeavesRangeTrace;
+	ns3::TracedCallback<Ptr<const SiotApplication>, Ptr<const MobilityModel>> m_nodeEntersRangeTrace;
+	ns3::TracedCallback<Ptr<const SiotApplication>, Ptr<const MobilityModel>> m_nodeLeavesRangeTrace;
 
 };
+
+template <typename Container>
+bool contains(Container const& c, typename Container::const_reference v) {
+  return std::find(c.begin(), c.end(), v) != c.end();
+}
+
+template<typename T>
+void remove(std::set<T> & v, const T & item)
+{
+	for (auto it = v.begin(); it != v.end(); /* "it" updated inside loop body */ )
+	{
+	    if ( *it == item )
+	    {
+	        // Erase the element matching the specified condition
+	        // from the associative container.
+	        it = v.erase(it);
+
+	        // Note:
+	        // erase() returns an iterator to the element
+	        // that follows the last element removed,
+	        // so we can continue the "for" loop iteration from that position.
+	    }
+	    else
+	    {
+	        // Current element does _not_ satisfy erasing condition,
+	        // so we can just move on to the next element.
+	        ++it;
+	    }
+	}
+}
 
 } /* namespace ns3 */
 
